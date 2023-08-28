e78d279b3d16239b62d735e4db7017cc8ff965d5
HeoJoonHo1 <billythur@naver.com>
2023-08-28 09:09:24 +0900

    plz

---
+++ src/Zero/Test02.java
@@ -0,0 +1,20 @@
+package Zero;
+
+public class Test02 {
+    //두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+    public static int solution(int a, int b, boolean flag) {
+        int answer = 0;
+
+        if (flag) {
+            answer = a + b;
+        } else {
+            answer = a - b;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        solution(3, 1, false);
+        System.out.println(solution(3, 1, true));
+    }
+}
