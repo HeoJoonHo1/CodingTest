11e9311cb9b5714cbeb3e5905e1b754ee0666c21
HeoJoonHo1 <billythur@naver.com>
2023-08-28 09:25:20 +0900

    plaaa

--- src/Zero/Test01.java
+++ src/Zero/Test01.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Test01 {
     //문자열 str이 주어질때 출력하는 코드를 작성해보세요.
-    //제한사항 1<= str <= 1000000 이 나와야 한다.sdsd
+    //제한사항 1<= str <= 1000000 이 나와야 한다.sdsdsssss
     //https://park-youjin.tistory.com/16
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
